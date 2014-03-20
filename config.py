67c11b57442b08dc08c32014e568f8c67c4e1caa
Mansoor Khan <thisismansoorkhan@gmail.com>
2014-03-20 14:13:19 +0530

    Petty change

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ SECRET_KEY = 'fhg503SLd34SFFKA45876VMB667'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'mysql://mansoor:mansoor@localhost/panSocial'
+SQLALCHEMY_DATABASE_URI = 'mysql://password:username@localhost/panSocial'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
